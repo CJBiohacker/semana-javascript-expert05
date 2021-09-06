a91699cd96c4ae7b17950bd35bc7cf7cc09b5778
CJBiohacker <cj.moyses@gmail.com>
2021-09-06 15:00:25 -0300

    " Setup do Jest e limpeza no config.mjs"

---
+++ src/gdriveclone-webapi/jest.config.mjs
@@ -0,0 +1,53 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * http
+ */
+
+export default {
+
+  clearMocks: true,
+
+  collectCoverage: true,
+
+  coverageDirectory: "coverage",
+
+  coverageProvider: "v8",
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
