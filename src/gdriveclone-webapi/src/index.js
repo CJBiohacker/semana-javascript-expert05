a35030dd6683b797332626ec4eba70f5187259ca
CJBiohacker <cj.moyses@gmail.com>
2021-09-06 17:45:05 -0300

    "Server Configurado p/ Teste"

--- src/gdriveclone-webapi/src/index.js
+++ src/gdriveclone-webapi/src/index.js
@@ -1,10 +1,11 @@
 import https from 'https';                                  // Importando o módulo interno do node 'https'
-
+import fs from 'fs';                                        // Importando o módulo interno do node 'fs' (Nodejs as a File Server)
+import logger from './logger';                              // Importando a constante 'logger'
 
 const PORT = process.env.PORT || 3000                       // Constante que leva a especificação da porta 3000 quando solicitada.
 
 const localHostSSL = {                                      // Constante que recebe um objeto com os certificados de permissão SSL.
-    key: fs.readFileSync('./certificates/key.pem'),
+    key: fs.readFileSync('./certificates/key.pem'),         // Método 'fs.readFileSync' que faz a leitura da chave e do certificado de forma SÍNCRONA.
     cert: fs.readFileSync('./certificates/cert.pem'),
 }
 
@@ -17,4 +18,5 @@ const server = https.createServer(                          // Constante que rec
 
 const startServer = () => {                                 // Constante que recebe a instanciação do servidor criado.
     const { address, port } = server.address()
+    logger.info(`Aplicativo executando no https://${address}:${port}`)
 }
