aa455f12435ff56dd5e45e6967b811be6cbf9011
CJBiohacker <cj.moyses@gmail.com>
2021-09-06 15:58:17 -0300

    "Arquivo de teste exemplo Jest"

---
+++ src/gdriveclone-webapi/test/example.teste.js
@@ -0,0 +1,11 @@
+import {
+    describe,
+    test,
+    expect
+} from '@jest/global'
+
+describe('#Teste de execução básica do Jest', () => {
+    test('#teste teste', () => {
+        expect(true).toBeTruthy()
+    })
+})
