8d2c73c16c5736231ddef5541bdeddc154569a0a
CJBiohacker <cj.moyses@gmail.com>
2021-09-06 19:05:57 -0300

    "Instanciaçao do Socket.io"

--- src/gdriveclone-webapi/src/index.js
+++ src/gdriveclone-webapi/src/index.js
@@ -1,6 +1,7 @@
 import https from 'https';                                                  // Importando o módulo interno do node 'https'
 import fs from 'fs';                                                        // Importando o módulo interno do node 'fs' (Nodejs as a File Server)
 import { logger } from './logger.js';                                       // Importando a constante 'logger'
+import { Server } from 'socket.io';                                         // Importando o módulo de 'Server' do 'socket.io'
 
 const PORT = process.env.PORT || 3000                                       // Constante que leva a especificação da porta 3000 quando solicitada.
 
@@ -16,9 +17,20 @@ const server = https.createServer(                                          // C
     }
 )
 
+const io = new Server(server, {                                             // Constante que recebe um construtor para o 'Socket IO' e recebe o servidor criado 'server' acima.
+    cors: {
+        origin: '*',
+        credentials: false
+    }
+})
+
+io.on("conexão",
+    (socket) => logger.info(`alguém se conectou: ${socket.id}`))            // Instanciação do 'Socket.io' para conexão com usuário externo.
+
 const startServer = () => {                                                 // Constante que recebe a instanciação do servidor criado.
     const { address, port } = server.address()
     logger.info(`Aplicativo executando no https://${address}:${port}`)
 }
 
 server.listen(PORT, startServer);                                           // Cria o servidor como 'listener' sobre a porta 'PORT = 3000' e hostname 'startServer()'
+
