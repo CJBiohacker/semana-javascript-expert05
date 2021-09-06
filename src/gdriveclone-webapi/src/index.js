b03c17cc1469ee76b0a92fed4bcbddcd6b25d3a9
CJBiohacker <cj.moyses@gmail.com>
2021-09-06 17:57:56 -0300

    "Server Configurado p/ Teste"

--- src/gdriveclone-webapi/src/index.js
+++ src/gdriveclone-webapi/src/index.js
@@ -1,22 +1,24 @@
-import https from 'https';                                  // Importando o módulo interno do node 'https'
-import fs from 'fs';                                        // Importando o módulo interno do node 'fs' (Nodejs as a File Server)
-import logger from './logger';                              // Importando a constante 'logger'
+import https from 'https';                                                  // Importando o módulo interno do node 'https'
+import fs from 'fs';                                                        // Importando o módulo interno do node 'fs' (Nodejs as a File Server)
+import { logger } from './logger.js';                                       // Importando a constante 'logger'
 
-const PORT = process.env.PORT || 3000                       // Constante que leva a especificação da porta 3000 quando solicitada.
+const PORT = process.env.PORT || 3000                                       // Constante que leva a especificação da porta 3000 quando solicitada.
 
-const localHostSSL = {                                      // Constante que recebe um objeto com os certificados de permissão SSL.
-    key: fs.readFileSync('./certificates/key.pem'),         // Método 'fs.readFileSync' que faz a leitura da chave e do certificado de forma SÍNCRONA.
+const localHostSSL = {                                                      // Constante que recebe um objeto com os certificados de permissão SSL.
+    key: fs.readFileSync('./certificates/key.pem'),                         // Método 'fs.readFileSync' que faz a leitura da chave e do certificado de forma SÍNCRONA.
     cert: fs.readFileSync('./certificates/cert.pem'),
 }
 
-const server = https.createServer(                          // Constante que recebe as configurações de criação do servidor https. 
+const server = https.createServer(                                          // Constante que recebe as configurações de criação do servidor https. 
     localHostSSL,
-    (req, res) => {                                         // Teste de Request e Response básico.
+    (req, res) => {                                                         // Teste de Request e Response básico.
         res.end("Commencing Virtuous Mission... NOW !!!")
     }
 )
 
-const startServer = () => {                                 // Constante que recebe a instanciação do servidor criado.
+const startServer = () => {                                                 // Constante que recebe a instanciação do servidor criado.
     const { address, port } = server.address()
     logger.info(`Aplicativo executando no https://${address}:${port}`)
 }
+
+server.listen(PORT, startServer);                                           // Cria o servidor como 'listener' sobre a porta 'PORT = 3000' e hostname 'startServer()'
