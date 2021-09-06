0250d8639327b93d82c92be0605efbb2131661f4
CJBiohacker <cj.moyses@gmail.com>
2021-09-06 19:38:03 -0300

    "Configuração das Rotas Padrão p/ teste"

--- src/gdriveclone-webapi/src/routes.js
+++ src/gdriveclone-webapi/src/routes.js
@@ -1,6 +1,6 @@
-import { logger } from "./logger"                               // Importação do 'logger'.
+import { logger } from "./logger.js"                                                   // Importação do 'logger'.
 
-export default class Routes {                                   // Classe 'Routes' que representa arquivo de rotas do server.
+export default class Routes {                                                       // Classe 'Routes' que representa arquivo de rotas do server.
     io
     constructor() {
     }
@@ -9,26 +9,26 @@ export default class Routes {                                   // Classe 'Route
         this.io = io
     }
 
-    async defaultRoute(request, response) {                     // Método que faz o pedido e resposta entre server e usuário.
+    async defaultRoute(request, response) {                                         // Método que faz o pedido e resposta entre server e usuário.
         response.end(`DEFAULT_ROUTE: Olá Mundo 1 !!!`)
     }
 
-    async options(request, response) {                          // 
+    async options(request, response) {                                              // 
         response.writeHead(204)
         response.end(`OPTIONS: Olá Mundo 2 !!!`)
     }
 
-    async post(request, response) {                             // 
+    async post(request, response) {                                                 // 
         logger.info('post')
-        response.end(`POST`)
+        response.end()
     }
 
-    async get(request, response) {                              // 
+    async get(request, response) {                                                  // 
         logger.info('get')
-        response.end(`GET`)
+        response.end()
     }
 
-    handler(request, response) {                                // 
+    handler(request, response) {                                                    // 
         response.setHeader('Acess-Control-Allow-Origin', '*')
         const chosen = this[request.method.toLowerCase()] || this.defaultRoute
 
