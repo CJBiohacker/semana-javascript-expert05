8cf1a5999594d84f6b3b87e1366b5f6919c7e85c
CJBiohacker <cj.moyses@gmail.com>
2021-09-07 16:14:48 -0300

    "Conversão dos métodos https p/ arrow function"

--- src/gdriveclone-webapi/src/routes.js
+++ src/gdriveclone-webapi/src/routes.js
@@ -1,4 +1,4 @@
-import { logger } from "./logger.js"                                                   // Importação do 'logger'.
+import { logger } from "./logger.js"                                                // Importação do 'logger'.
 
 export default class Routes {                                                       // Classe 'Routes' que representa arquivo de rotas do server.
     io
@@ -9,30 +9,29 @@ export default class Routes {                                                   
         this.io = io
     }
 
-    async defaultRoute(request, response) {                                         // Método que faz a rota do pedido e resposta entre server e usuário.
+    defaultRoute = async (request, response) => {                                   // Método que faz a rota do pedido e resposta entre server e usuário.
         response.end(`DEFAULT_ROUTE: Olá Mundo 1 !!!`)
     }
 
-    async options(request, response) {                                              // Rota de pedido de método 'https OPTIONS'.
+    options = async (request, response) => {                                        // Rota de pedido de método 'https OPTIONS'.
         response.writeHead(204)
         response.end(`OPTIONS: Olá Mundo 2 !!!`)
     }
 
-    async post(request, response) {                                                 // Rota de pedido de método 'https POST'.
+    post = async (request, response) => {                                           // Rota de pedido de método 'https POST'.
         logger.info('post')
         response.end()
     }
 
-    async get(request, response) {                                                  // Rota de pedido de método 'https GET'.
+    get = async (request, response) => {                                            // Rota de pedido de método 'https GET'.
         logger.info('get')
         response.end()
     }
 
-    handler(request, response) {                                                    // 
+    handler = (request, response) => {                                              // Configuração de gerenciamento dos métodos (pedido,resposta) 
         response.setHeader('Acess-Control-Allow-Origin', '*')
-        const chosen = this[request.method.toLowerCase()] || this.defaultRoute
+        const chosen = this[request.method.toLowerCase()] || this.defaultRoute      // Conversão para 'lowerCase' para evitar conflito com o nome das funções de rota criadas.
 
-        chosen()
         return chosen.apply(this, [request, response])
     }
 }
