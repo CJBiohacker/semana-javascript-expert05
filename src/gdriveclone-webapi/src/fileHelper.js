f7edcd71739f697efd1d0b974f353033cf219dd2
CJBiohacker <cj.moyses@gmail.com>
2021-09-07 20:57:08 -0300

    "Configurando FileStatuses"

--- src/gdriveclone-webapi/src/fileHelper.js
+++ src/gdriveclone-webapi/src/fileHelper.js
@@ -4,12 +4,13 @@ import prettyBytes from 'pretty-bytes'
 export default class FileHelper {
     static async getFileStatus(downloadsFolder) {
         const currentFiles = await fs.promises.readdir(downloadsFolder)
+
         const statuses = await Promise.all(currentFiles.map(file =>
             fs.promises.stat(`${downloadsFolder}/${file}`)))
 
         for (const fileIndex in currentFiles) {
             const { birthtime, size } = statuses[fileIndex]
-            console.log({ birthtime, size: prettyBytes(size) });
+            console.log({ birthtime, size: prettyBytes(size) })
 
         }
     }
--- src/gdriveclone-webapi/test/unit/fileHelper.test.js
+++ src/gdriveclone-webapi/test/unit/fileHelper.test.js
@@ -10,28 +10,26 @@ describe('#FileHelper Test', () => {                                            
     describe('#getFiletStatus Test', () => {                                                   // Teste Jest do método 'getFiletStatus.
         test("it should return file statuses in correct", async () => {
 
-            const statMock = [
-                {
-                    dev: 66306,
-                    mode: 33204,
-                    nlink: 1,
-                    uid: 1000,
-                    gid: 1000,
-                    rdev: 0,
-                    blksize: 4096,
-                    ino: 1712779,
-                    size: 499719,
-                    blocks: 984,
-                    atimeMs: 1631051910007.6746,
-                    mtimeMs: 1610410847000,
-                    ctimeMs: 1631051904095.6487,
-                    birthtimeMs: 1628625145477.3994,
-                    atime: '2021-09-07T21:58:30.008Z',
-                    mtime: '2021-01-12T00:20:47.000Z',
-                    ctime: '2021-09-07T21:58:24.096Z',
-                    birthtime: '2021-08-10T19:52:25.477Z'
-                }
-            ]
+            const statMock = {
+                dev: 66306,
+                mode: 33204,
+                nlink: 1,
+                uid: 1000,
+                gid: 1000,
+                rdev: 0,
+                blksize: 4096,
+                ino: 1712779,
+                size: 499719,
+                blocks: 984,
+                atimeMs: 1631051910007.6746,
+                mtimeMs: 1610410847000,
+                ctimeMs: 1631051904095.6487,
+                birthtimeMs: 1628625145477.3994,
+                atime: '2021-09-07T21:58:30.008Z',
+                mtime: '2021-01-12T00:20:47.000Z',
+                ctime: '2021-09-07T21:58:24.096Z',
+                birthtime: '2021-08-10T19:52:25.477Z'
+            }
 
             const mockUser = 'carlos'
             process.env.USER = mockUser
