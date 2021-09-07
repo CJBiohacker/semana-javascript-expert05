07167428b73ec29e2427cd6f5a50af67e7fb7a1d
CJBiohacker <cj.moyses@gmail.com>
2021-09-07 16:51:10 -0300

    " Teste do método setSocketInstance "

--- src/gdriveclone-webapi/test/unit/route.test.js
+++ src/gdriveclone-webapi/test/unit/route.test.js
@@ -1,11 +1,26 @@
-import {                                                        // Importando os 3 métodos globais do Jest: 'describe', 'teste' e 'expect'; 
-    describe,
-    test,
-    expect
-} from '@jest/globals'
+import { describe, test, expect } from '@jest/globals'              // Importando os 3 métodos globais do Jest: 'describe', 'teste' e 'expect'; 
+import Routes from '../../src/routes.js';                           // Importando do Routes.
 
-describe('#Teste de execução básica do Jest', () => {           // Método global 'describe' do Jest que cria um bloco para agrupar testes.
-    test('#teste teste', () => {
-        expect(true).toBeTruthy()                               // true = Pass | false = Failed
+describe('#Jest Basic Test', () => {                                // Método global 'describe' do Jest que cria um bloco para agrupar testes.
+    test('#Test 1', () => {
+        expect(true).toBeTruthy()                                   // true = Pass | false = Failed
     })
 })
+
+describe('#Route Test Suite', () => {                               // Teste do método 'setSocketInstance' declarado através de um construtor de classe.
+    describe('#setSocketInstance Test', () => {
+        test("#setSocket should store 'io' instance", () => {
+            const routes = new Routes()
+            const ioObj = {
+                to: (id) => ioObj,
+                emit: (event, message) => { }
+            }
+            routes.setSocketInstance(ioObj)
+            expect(routes.io).toStrictEqual(ioObj)
+        })
+        test('#Test 1', () => {
+            expect(true).toBeTruthy()
+        })
+    })
+})
+
